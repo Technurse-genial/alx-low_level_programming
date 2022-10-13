b3e0e5e0c9b2882267a9a9087a9001873eda62d5
Technurse-genial <owoidigheabasieffiong@gmail.com>
2022-10-13 09:07:31 -0700

    Task 6-Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  *main-printing
+  *Return: Always 0
+  */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n", sizeof(char));
+	printf("Size of an int: 4 byte(s)\n", sizeof(int));
+	printf("Size of long int: 4 byte(s)\n", sizeof(long int));
+	printf("Size of long int: 4 byte(s)\n", sizeof(long long int));
+	printf("Size of a float: 4 byte(s)\n", sizeof(float));
+	return (0);
