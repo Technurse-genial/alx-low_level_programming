3bcf166ab32518ba6f16d64fc0c3bf2409aeaaa9
Technurse-genial <owoidigheabasieffiong@gmail.com>
2022-10-13 10:21:35 -0700

    Task 6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
-  *main-printing
-  *Return: Always 1
-  */
+   *main-program entry point.
+    *
+     *Return: 1 on success.
+      */
 int main(void)
 {
-	write(2, "and that piece of art is useful"- Dora Korpar, 2015-10-19;return (1);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
