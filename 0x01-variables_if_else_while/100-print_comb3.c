83334bf51c014db95205a218aa59cecd8c683481
Technurse-genial <owoidigheabasieffiong@gmail.com>
2022-10-15 14:22:02 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+/**
+  *main - print a-z
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			if (i != j && i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (j != 57 || i != 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
 
