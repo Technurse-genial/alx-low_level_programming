30f572bb5ce0637fe6b88a3f06dcf745c6d934cd
Technurse-genial <owoidigheabasieffiong@gmail.com>
2022-10-18 05:15:14 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,15 +3,16 @@
   *print_alphabet_x10 - prints 10 times the alphabet, in lowercase
   *Return: 0
   */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
-	int count = 0;
-	char letter;
+	int i, j;
 
-	while (count++ <= 9)
+	for (i = 0; i < 10; i++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
 		_putchar('\n');
 	}
 }
